bb593041d7452bea611b6f1f3cdeba254413a55f
Jingnong <jingnongwang@gmail.com>
2019-03-15 00:51:33 -0700

    breaking change: master logic with Tong

--- Master_controller.py
+++ Master_controller.py
@@ -29,8 +29,8 @@ class Master_controller(threading.Thread):
         self.motor_1_direction = 0  # 电机转动方向， 0--stop 1--Go 2--back
         self.motor_2_direction = 0
         self.game_dot_queue = game_dot_queue
-        self.Last_direction_x = 1
-        self.Last_direction_y = 1
+        self.Last_direction_x = 0
+        self.Last_direction_y = 0
         self.motor = Motor.Motor()
         self.time_1 = time.time()
         self.time_2 = time.time()
@@ -46,8 +46,6 @@ class Master_controller(threading.Thread):
         # producer-consumer queue for AD input for tablet and joystick
         # joystick_x_queue = queue.Queue()
         # joystick_y_queue = queue.Queue()
-
-
 
         # 注册GPIO: 电机-2-OUT，霍尔开关-2-IN，平板-2-OUT
         GPIO.setup(self.HALL_1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -73,6 +71,9 @@ class Master_controller(threading.Thread):
 
         while True:
 
+            #         jx, jy, h1t, h1c, h2t, h2c, m1, m2, time_enter, time_exit, loop_time
+            status = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
             time_3 = time.time()
             '''
             if (not joystick_x_queue.empty()) and (not joystick_y_queue.empty()):
@@ -92,57 +93,97 @@ class Master_controller(threading.Thread):
 
             joystick_x = ads1256_controller.ADS1256_GetoneChannel(3)
             joystick_y = ads1256_controller.ADS1256_GetoneChannel(4)
-            self.hall_1_target = (joystick_x - JOYSTICK_X_MIN - JOYSTICK_X_MID) / (JOYSTICK_X_MAX - JOYSTICK_X_MIN) * HALL_1_MAX
-            self.hall_2_target = (joystick_y - JOYSTICK_Y_MIN - JOYSTICK_Y_MID) / (JOYSTICK_Y_MAX - JOYSTICK_Y_MIN) * HALL_2_MAX
-            print("jx=", joystick_x, " jy=", joystick_y, " h1t=", int(self.hall_1_target), "h2t=", int(self.hall_2_target), "h1c=", self.hall_1_counter, "h2c=", self.hall_2_counter)
+            status[0] = joystick_x
+            status[1] = joystick_y
+            self.hall_1_target = (joystick_x - JOYSTICK_X_MIN - JOYSTICK_X_MID) / (
+                        JOYSTICK_X_MAX - JOYSTICK_X_MIN) * HALL_1_MAX
+            self.hall_2_target = (joystick_y - JOYSTICK_Y_MIN - JOYSTICK_Y_MID) / (
+                        JOYSTICK_Y_MAX - JOYSTICK_Y_MIN) * HALL_2_MAX
+            status[2] = self.hall_1_target
+            status[4] = self.hall_2_target
 
             # 驱动电机
-            if self.hall_1_counter < (self.hall_1_target - 1):
+            status[3] = self.hall_1_counter
+            if status[3] < (self.hall_1_target - 1):
                 cur_time = time.time()
-                if not (self.motor_1_direction == 2 and cur_time - self.time_1 < 1):
+                status[6] = 1
+                if (self.motor_1_direction == 2):
+                    self.motor.Stop_1()
+                    self.Last_direction_x = 2
+                    self.motor_1_direction = 0
+                    self.time_1 = time.time()
+                    status[6] = 2
+                elif not (self.Last_direction_x == 2 and cur_time - self.time_1 < 0.1):
                     self.motor.Go_1()
                     self.motor_1_direction = 1
-                    print("Go_1", " h1t=", self.hall_1_target, " h1c=", self.hall_1_counter)
-            elif self.hall_1_counter > (self.hall_1_target + 1):
+                    self.Last_direction_x = 0
+                    status[6] = 3
+            elif status[3] > (self.hall_1_target + 1):
                 cur_time = time.time()
-                if not (self.motor_1_direction == 1 and cur_time - self.time_1 < 1):
+                status[6] = 4
+                if (self.motor_1_direction == 1):
+                    self.motor.Stop_1()
+                    self.Last_direction_x = 1
+                    self.motor_1_direction = 0
+                    self.time_1 = time.time()
+                    status[6] = 5
+                elif not (self.motor_1_direction == 1 and cur_time - self.time_1 < 0.1):
                     self.motor.Back_1()
                     self.motor_1_direction = 2
-                    print("Back_1", " h1t=", self.hall_1_target, " h1c=", self.hall_1_counter)
+                    self.Last_direction_x = 0
+                    status[6] = 6
+            else:
+                # if self.my_equal(self.hall_1_counter, self.hall_1_target):
+                self.motor.Stop_1()
+                status[6] = 7
+                if (self.motor_1_direction != 0):
+                    self.Last_direction_x = self.motor_1_direction
+                    self.motor_1_direction = 0
+                    self.time_1 = time.time()
+                    status[6] = 8
 
-            if self.hall_2_counter < (self.hall_2_target - 1):
+            status[5] = self.hall_2_counter
+            if status[5] < (self.hall_2_target - 1):
                 cur_time = time.time()
-                if not (self.motor_2_direction == 2 and cur_time - self.time_2 < 1):
+                status[7] = 1
+                if (self.motor_2_direction == 2):
+                    self.motor.Stop_2()
+                    self.Last_direction_y = 2
+                    self.motor_2_direction = 0
+                    self.time_2 = time.time()
+                    status[7] = 2
+                elif not (self.motor_2_direction == 2 and cur_time - self.time_2 < 0.1):
                     self.motor.Go_2()
                     self.motor_2_direction = 1
-                    print("Go_2", " h2t=", self.hall_2_target, " h2c=", self.hall_2_counter)
-            elif self.hall_2_counter > (self.hall_2_target + 1):
+                    self.Last_direction_y = 0
+                    status[7] = 3
+            elif status[5] > (self.hall_2_target + 1):
                 cur_time = time.time()
-                if not (self.motor_2_direction == 1 and cur_time - self.time_2 < 1):
+                status[7] = 4
+                if (self.motor_2_direction == 1):
+                    self.motor.Stop_2()
+                    self.Last_direction_y = 1
+                    self.motor_2_direction = 0
+                    self.time_2 = time.time()
+                    status[7] = 5
+                elif not (self.motor_2_direction == 1 and cur_time - self.time_2 < 0.1):
                     self.motor.Back_2()
                     self.motor_2_direction = 2
-                    print("Back_2", " h2t=", self.hall_2_target, " h2c=", self.hall_2_counter)
+                    self.Last_direction_y = 0
+                    status[7] = 6
+            else:
+                # if self.my_equal(self.hall_2_counter, self.hall_2_target):
+                self.motor.Stop_2()
+                status[7] = 7
+            if (self.motor_2_direction != 0):
+                self.Last_direction_y = self.motor_2_direction
+                self.motor_2_direction = 0
+                self.time_2 = time.time()
+                status[7] = 8
 
-            # if self.my_equal(self.hall_1_counter, self.hall_1_target):
-            #     motor.Stop_1()
-            #     if (self.motor_1_direction != 0):
-            #         self.Last_direction_x = self.motor_1_direction
-            #         self.motor_1_direction = 0
-            #         time_1 = time.time()
-            #     # print("motor.Stop_1, counter reached target, ", "hall_1_counter = ", self.hall_1_counter, ", hall_1_target = ", int(hall_1_target))
-            #     print("Stop_1 ", "h1c=", self.hall_1_counter, " h1t=", int(self.hall_1_target))
-
-            # if self.my_equal(self.hall_2_counter, self.hall_2_target):
-            #     self.motor.Stop_2()
-            #     if (self.motor_2_direction != 0):
-            #         self.Last_direction_y = self.motor_2_direction
-            #         self.motor_2_direction = 0
-            #         time_2 = time.time()
-            #     # print("Stop_2, counter reached target, ", "hall_2_counter = ", self.hall_2_counter, ", hall_2_target = ", int(hall_2_target))
-            #     print("Stop_2 ", "h2c=", self.hall_2_counter, " h2t=", int(self.hall_2_target))
-
-            loop_count = loop_count + 1
-            print("loop count=", loop_count, " loop time=", time.time() - time_3)
+            status[8] = round(time_3, 5)
+            status[9] = round(time.time(), 5)
+            print(status)
 
     # 霍尔开关线程
     def hall_1_callback(self, channel1):
@@ -159,15 +200,6 @@ class Master_controller(threading.Thread):
                 elif (self.Last_direction_x == 2):
                     self.hall_1_counter = self.hall_1_counter - 1
 
-            if self.my_equal(self.hall_1_counter, self.hall_1_target):
-                self.motor.Stop_1()
-                if (self.motor_1_direction != 0):
-                    self.Last_direction_x = self.motor_1_direction
-                    self.motor_1_direction = 0
-                    self.time_1 = time.time()
-                # print("motor.Stop_1, counter reached target, ", "hall_1_counter = ", self.hall_1_counter, ", hall_1_target = ", int(hall_1_target))
-                print("Stop_1 ", "h1c=", self.hall_1_counter, " h1t=", int(self.hall_1_target))
-
     def hall_2_callback(self, channel2):
         if GPIO.event_detected(self.HALL_2_PIN):
             if self.motor_2_direction == 1:
@@ -181,15 +213,6 @@ class Master_controller(threading.Thread):
                     self.hall_2_counter = self.hall_2_counter + 1
                 elif (self.Last_direction_y == 2):
                     self.hall_2_counter = self.hall_2_counter - 1
-
-            if self.my_equal(self.hall_2_counter, self.hall_2_target):
-                self.motor.Stop_2()
-                if (self.motor_2_direction != 0):
-                    self.Last_direction_y = self.motor_2_direction
-                    self.motor_2_direction = 0
-                    self.time_2 = time.time()
-                # print("Stop_2, counter reached target, ", "hall_2_counter = ", self.hall_2_counter, ", hall_2_target = ", int(hall_2_target))
-                print("Stop_2 ", "h2c=", self.hall_2_counter, " h2t=", int(self.hall_2_target))
 
     # 留有余量的比较
     def my_equal(self, a, b):
